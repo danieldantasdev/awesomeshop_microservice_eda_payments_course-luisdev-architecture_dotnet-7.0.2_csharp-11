a103ee76262b3be8ee8c326389b4204c6cc02005
danieldantasdev <contatolobinhodev@gmail.com>
2023-08-06 21:51:38 -0300

    feat: add dependency injection services

--- AwesomeShop.Services.Orders.Api/Program.cs
+++ AwesomeShop.Services.Orders.Api/Program.cs
@@ -1,3 +1,9 @@
+using AwesomeShop.Application.Subscribers;
+using AwesomeShop.Core.MessageBus.Interfaces;
+using AwesomeShop.Core.Repositories.Interfaces;
+using AwesomeShop.Infrastructure.Persistence.Repositories.Implementations;
+using AwesomeShop.Infrastructure.Services.Implementations.MessageBus.RabbitMq;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IMessageBusService, RabbitMqService>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+builder.Services.AddHostedService<OrderShippedSubscriber>();
 
 var app = builder.Build();
 
